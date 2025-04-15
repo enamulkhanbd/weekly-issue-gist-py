ee8ec6f96a660edf3a5689c169f3e2f2fd5a3162
enamulkhanbd <enamulkhanbd@yahoo.com>
2025-04-16 02:49:33 +0600

    final commit

--- weekly_issues_gist_email.py
+++ weekly_issues_gist_email.py
@@ -2,33 +2,26 @@ from dotenv import load_dotenv
 load_dotenv()
 
 import requests
-import datetime
+from datetime import datetime
 import smtplib
 from email.message import EmailMessage
 import os
 
 # CONFIGURATION
 GITHUB_TOKEN = os.environ['GITHUB_TOKEN']
-REPO = "enamulkhanbd/weekly-issue-gist-py.git"
-DEVELOPER_EMAIL = "fuhadhasan472@gmail.com"
+REPO = "enamulkhanbd/weekly-issue-gist-py"
+DEVELOPER_EMAIL = ["fuhadhasan472@gmail.com, fuhadhasan479@gmail.com"]
 SENDER_EMAIL = "hometv.ipa@gmail.com"
 SENDER_EMAIL_PASSWORD = os.environ['EMAIL_PASSWORD']  # Use App Password for Gmail
 SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 587
 
 def get_last_week_issues():
-    print("🟡 get_last_week_issues() called")
-
-    token = os.getenv("GITHUB_TOKEN")
-    if not token:
-        raise Exception("❌ GITHUB_TOKEN is missing from environment variables.")
-
-    print("✅ Token exists")
-
-    one_week_ago = (datetime.datetime.utcnow() - datetime.timedelta(days=7)).isoformat() + "Z"
+    # one_week_ago = (datetime.datetime.utcnow() - datetime.timedelta(days=7)).isoformat() + "Z"
+    today_start = datetime.utcnow().replace(hour=0, minute=0, second=0, microsecond=0).isoformat() + "Z"
     url = f"https://api.github.com/repos/{REPO}/issues"
     headers = {"Authorization": f"token {GITHUB_TOKEN}"}
-    params = {"since": one_week_ago, "state": "all"}
+    params = {"since": today_start, "state": "all"}
 
     response = requests.get(url, headers=headers, params=params)
 
@@ -62,7 +55,7 @@ def send_email(gist_url):
     msg["Subject"] = "📝 Weekly GitHub Issue Summary"
     msg["From"] = SENDER_EMAIL
     msg["To"] = DEVELOPER_EMAIL
-    msg.set_content(f"Hi Dev,\n\nHere's the weekly GitHub issue summary:\n\n{gist_url}\n\nCheers!")
+    msg.set_content(f"Hello Dev,\n\nHere's the weekly GitHub issue summary:\n\n{gist_url}\n\nCheers!")
 
     with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
         server.starttls()
